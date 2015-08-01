7abfb0ee1141d1784bab787c6c71d69453853bea
Adrien Bouttier <hibigbob@gmail.com>
2015-08-01 13:53:47 +0200

    show list on sidebar

--- src/components/List.js
+++ src/components/List.js
@@ -3,6 +3,16 @@ import AuthenticatedComponent from './AuthenticatedComponent';
 import ListStore from '../stores/ListStore.js';
 import ListService from '../services/ListService.js';
 
+var OneList = React.createClass({
+  render() {
+    return (
+      <li>
+        <a className="nav-submenu" data-toggle="nav-submenu" href="#"><span className="sidebar-mini-hide">{this.props.list.name}</span></a>
+      </li>
+    );
+  }
+});
+
 export default AuthenticatedComponent(class List extends React.Component {
   constructor(props) {
     super(props);
@@ -37,8 +47,21 @@ export default AuthenticatedComponent(class List extends React.Component {
   }
 
   render() {
-    return (
+    var lists = this.state.list;
+    return (<ul className="nav-main">
+      <li>
+          <a className="active" href="index.html"><i className="si si-speedometer"></i><span className="sidebar-mini-hide">Dashboard {this.props.user.username}</span></a>
+      </li>
       <li className="nav-main-heading"><span className="sidebar-mini-hide">List</span></li>
-    );
+      {
+        Object.keys(lists).map(function (key) {
+          var list = lists[key];
+          return (
+            <OneList list={list} />
+          );
+        })
+      }
+      </ul>
+    )
   }
 });
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,16 +15,11 @@ export default class Sidebar extends React.Component {
                           <i className="fa fa-times"></i>
                       </button>
                       <a className="h5 text-white" href="index.html">
-                          <i className="fa fa-circle-o-notch text-primary"></i> <span className="h4 font-w600 sidebar-mini-hide">ne</span>
+                          <i className="fa fa-check-square text-primary"></i> <span className="h5 font-w600 sidebar-mini-hide">brain</span>
                       </a>
                   </div>
                   <div className="side-content">
-                      <ul className="nav-main">
-                          <li>
-                              <a className="active" href="index.html"><i className="si si-speedometer"></i><span className="sidebar-mini-hide">Dashboard {this.props.user.username}</span></a>
-                          </li>
-                          <List />
-                      </ul>
+                      <List user={this.props.user} />
                   </div>
               </div>
           </div>
