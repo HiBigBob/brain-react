cd8689d17ec71f0412c1bf38324a70554a124720
Adrien Bouttier <hibigbob@gmail.com>
2015-08-10 22:13:03 +0200

    clear style list

--- src/components/List.js
+++ src/components/List.js
@@ -5,7 +5,7 @@ export default AuthenticatedComponent(class List extends React.Component {
   render() {
     return (
       <li onClick={this.props.onClick}>
-        <a className="nav-submenu" data-toggle="nav-submenu" href="#"><span className="sidebar-mini-hide">{this.props.list.name}</span></a>
+        <a href="#">{this.props.list.name}</a>
       </li>
     );
   }
