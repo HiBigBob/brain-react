d546420d626d4f2adca17aa2ff7d72c846e77b9a
Adrien Bouttier <hibigbob@gmail.com>
2015-08-12 14:13:01 +0200

    add new action to task

--- src/actions/TaskActions.js
+++ src/actions/TaskActions.js
@@ -1,10 +1,16 @@
 import AppDispatcher from '../dispatchers/AppDispatcher';
-import {TASK_GET} from '../constants/TaskConstants';
+import {TASK_GET, TASK_ADD} from '../constants/TaskConstants';
 
 export default {
   getTask: (task) => {
     AppDispatcher.dispatch({
       actionType: TASK_GET,
+      task: task
+    })
+  },
+  addTask: (task) => {
+    AppDispatcher.dispatch({
+      actionType: TASK_ADD,
       task: task
     })
   }
