b78e1509bc7cd5b09a4c2764a8560e4868cc35d8
Adrien Bouttier <hibigbob@gmail.com>
2015-08-13 21:10:00 +0200

    Add constants

--- src/constants/TaskConstants.js
+++ src/constants/TaskConstants.js
@@ -2,5 +2,6 @@ var BASE_URL = 'http://localhost:3000/';
 export default {
   BASE_URL: BASE_URL,
   TASK_URL: BASE_URL + 'api/tasks',
-  TASK_GET: 'TASK_GET'
+  TASK_GET: 'TASK_GET',
+  TASK_ADD: 'TASK_ADD'
 }
