0da882b3a49cb9b2b802ecc660b0e8c8e06f25ba
Adrien Bouttier <hibigbob@gmail.com>
2015-09-05 16:38:51 +0200

    Align adding button

--- src/components/Task.js
+++ src/components/Task.js
@@ -266,7 +266,7 @@ export default AuthenticatedComponent(class Task extends React.Component {
                           <div className="block-content">
                               <div className="push">
                                   <div className="btn-group">
-                                      <button className="btn btn-default btn-xs push-50-r push-20-l" type="button" onClick={this.toggleAdd.bind(this, !this.state.showAdd)}><i className="fa fa-plus fa-fw"></i></button>
+                                      <button className="btn btn-default btn-xs push-50-r push-30-l" type="button" onClick={this.toggleAdd.bind(this, !this.state.showAdd)}><i className="fa fa-plus fa-fw"></i></button>
                                       <button className="btn btn-default btn-xs" type="button" onClick={this.toggleFilter.bind(this, true, true)}><i className={classFilterAll}></i> <span className="hidden-xs">All</span></button>
                                       <button className="btn btn-default btn-xs" type="button" onClick={this.toggleFilter.bind(this, !this.state.filter.active, this.state.filter.completed)}><i className={classFilterActive}></i> <span className="hidden-xs">Active</span></button>
                                       <button className="btn btn-default btn-xs" type="button" onClick={this.toggleFilter.bind(this, this.state.filter.active, !this.state.filter.completed)}><i className={classFilterCompleted}></i> <span className="hidden-xs">Completed</span></button>
